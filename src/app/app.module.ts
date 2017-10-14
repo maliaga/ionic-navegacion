f3e01880d16bc7a9b16fd809396f6c3d18275137
Mario Aliaga <ma@marioaliaga.com>
2017-10-13 23:56:41 -0300

    Cambio la página de root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 
 import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
 
 import {
   AjustesPage,
@@ -20,7 +19,6 @@ import {
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
     AjustesPage,
     Ajustes2Page,
     ModalPage,
@@ -36,7 +34,6 @@ import {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
     AjustesPage,
     Ajustes2Page,
     ModalPage,
