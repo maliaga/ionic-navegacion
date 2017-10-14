647032e32025cd75a048e06d590a22bbf00c619e
Mario Aliaga <ma@marioaliaga.com>
2017-10-14 00:26:50 -0300

    Agrego enviar y recibir parametros

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the Pagina2Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import { Pagina3Page } from "../index.paginas"
 @Component({
   selector: 'page-pagina2',
   templateUrl: 'pagina2.html',
 })
 export class Pagina2Page {
+
+  pagina3:any = Pagina3Page;
+
+  mutantes: any[] = [
+    {
+      nombre: "Magneto",
+      poder: "Controlar metales"
+    },
+    {
+      nombre: "Wolverine",
+      poder: "Regeneración acelerada"
+    },
+    {
+      nombre: "Profesor X",
+      poder: "Poderes Psiquicos"
+    }
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -21,4 +31,8 @@ export class Pagina2Page {
     console.log('ionViewDidLoad Pagina2Page');
   }
 
+  irPagina3(mutante: any) {
+    console.log(mutante);
+    this.navCtrl.push(Pagina3Page,{mutante});
+  }
 }
